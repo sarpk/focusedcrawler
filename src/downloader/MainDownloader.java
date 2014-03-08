fd3aa5d41c793711913206f807ff4c4d5052c123
Sarp Kaya <sarpkaya@live.com.au>
2014-03-08 17:08:13 +1000

    MainDownloader class for downloader package is added, which controls Download and Parse classes

--- src/downloader/MainDownloader.java
+++ src/downloader/MainDownloader.java
@@ -1,12 +1,43 @@
 package downloader;
 
+import java.util.LinkedHashMap;
+
 /**
  * Main downloader class that drives downloading process
  * @author Sarp
  *
  */
 public class MainDownloader {
-	MainDownloader(String address) {
-		
+	private Download download;
+	private Parse parser;
+	/**
+	 * Constructor for the Main Downloader
+	 * @param address to download the address
+	 */
+	public MainDownloader(String address) {
+		download = new Download(address);
+		if(didDownloadFinish()) {
+			parser = new Parse (download.getPageContent());
+		}
 	}
+	/**
+	 * @return True if download is finished
+	 */
+	public boolean didDownloadFinish() {
+		return download.didDownloadFinish();
+	}
+	/**
+	 * @return Body of page
+	 */
+	public String getPageBody() {
+		return parser.getPageBody();
+	}
+	
+	/**
+	 * @return Clickable(href) link map, key is URL, value is the content
+	 */
+	public LinkedHashMap<String, String> getClickableLinks() {
+		return parser.getLinks();
+	}
+	
 }
