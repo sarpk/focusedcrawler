0dcdad3482388abbdd05c508d372782534699585
Sarp Kaya <sarpkaya@live.com.au>
2014-03-12 00:50:36 +1000

    Crawler can download page and also printing hyperlinks for checking

--- src/crawler/MainCrawler.java
+++ src/crawler/MainCrawler.java
@@ -1,12 +1,33 @@
 package crawler;
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+import downloader.MainDownloader;
+
 /**
  * Main Crawler Class that drives the crawler
+ * 
  * @author Sarp
- *
+ * 
  */
 public class MainCrawler {
 
+	/**
+	 * Crawls the given page
+	 * @param address of the website
+	 */
 	public void crawl(String address) {
-		
+		MainDownloader mDownloader = new MainDownloader(address);
+		if (mDownloader.didDownloadFinish()) {
+			System.out.println(mDownloader.getPageBody() + "\nLinks are:\n");
+			LinkedHashMap<String, String> map = mDownloader.getClickableLinks();
+			if (map != null) {
+				for (Entry<String, String> pair : map.entrySet()) {
+					System.out.println(pair.getKey() + " val is: "
+							+ pair.getValue());
+				}
+			}
+		}
 	}
 }
